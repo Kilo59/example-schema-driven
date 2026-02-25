81757538ac40b5f3125cfa4463caf52c750d47bf
Gabriel G <gabriel59kg@gmail.com>
2026-02-25 07:52:50 -0500

    Revert "Refactors application shutdown on interrupt"
    This reverts commit 68aa6eff4092ff27b358d202e41430475a60949a.

--- duties.py
+++ duties.py
@@ -1,8 +1,8 @@
 import subprocess
+import sys
 
 from duty import duty
 from duty.context import Context
-from duty.exceptions import DutyError
 
 
 @duty
@@ -60,8 +60,8 @@ def start(ctx: Context):
     try:
         backend_proc.wait()
         frontend_proc.wait()
-    except KeyboardInterrupt as e:
+    except KeyboardInterrupt:
         print("\n[INFO] Shutting down application processes...")
         backend_proc.terminate()
         frontend_proc.terminate()
-        raise DutyError("Application stopped by user") from e
+        sys.exit(0)
